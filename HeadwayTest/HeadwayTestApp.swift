f47a67797be20c0b6c01c19a38907b4811e2d38d
Danil <dashc@ciklum.com>
2022-08-01 12:45:04 +0300

    Initial Commit

---
+++ HeadwayTest/HeadwayTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayTestApp.swift
+//  HeadwayTest
+//
+//  Created by Danil on 01.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeadwayTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
